9445370bc8fc98403716c22e36a489fdae53e466
mireu-lab <limmireu1214@gmail.com>
2024-04-14 00:30:45 +0900

    rename 1 file, create 3 files and update 1 file

---
+++ app/main.go
@@ -0,0 +1,16 @@
+package main
+
+import (
+	"time"
+
+	"github.com/gin-gonic/gin"
+)
+
+func main() {
+	router := gin.Default()
+
+	router.GET("/", func(c *gin.Context) {
+		c.JSONP(200, gin.H{"msg": "pong", "time": time.RFC3339})
+	})
+	router.Run(":80")
+}
